069057e3b626ddee418cc455886a740ef354a492
Arlen Pan <arlenpan@gmail.com>
2022-05-15 18:57:03 -0700

    set up linting and prettier, absolute imports

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Hi Ying ur my bubba</Text>
+            <Text>your BUTTTTT is nice</Text>
+            <Image
+                style={styles.image}
+                source={{
+                    uri: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZe4g2c3QZMbVPeRcohes8fesioiPM0FnxhA&usqp=CAU`,
+                }}
+            />
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image: {
+        width: 300,
+        height: 300,
+    },
 });
